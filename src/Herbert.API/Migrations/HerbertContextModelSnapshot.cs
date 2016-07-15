b4378b56f831e672df230691af290ad9adf6ed8a
BigEgg <cd_bigegg@hotmail.com>
2016-07-15 20:46:08 +0800

    Add first db migration script

---
+++ src/Herbert.API/Migrations/HerbertContextModelSnapshot.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Herbert.Data;
+
+namespace Herbert.API.Migrations
+{
+    [DbContext(typeof(HerbertContext))]
+    partial class HerbertContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.0-rtm-21431")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("Herbert.Model.UserInfo.ApplicationUser", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("CreatedTime")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Email")
+                        .IsConcurrencyToken()
+                        .IsRequired()
+                        .HasAnnotation("MaxLength", 255);
+
+                    b.Property<DateTime>("LastUpdated")
+                        .ValueGeneratedOnAddOrUpdate();
+
+                    b.Property<string>("NickName")
+                        .IsRequired();
+
+                    b.Property<string>("Password")
+                        .IsRequired();
+
+                    b.Property<int>("RegisterSource");
+
+                    b.Property<int>("Role");
+
+                    b.HasKey("Id");
+
+                    b.HasAlternateKey("Email");
+
+                    b.HasIndex("Email");
+
+                    b.ToTable("ApplicationUsers");
+                });
+        }
+    }
+}
