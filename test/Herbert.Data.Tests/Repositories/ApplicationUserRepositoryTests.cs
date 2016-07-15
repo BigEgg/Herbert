a54c864526f1d523d724643608df99fdb3539f03
BigEgg <cd_bigegg@hotmail.com>
2016-07-16 00:58:58 +0800

    Add the Data layer test project

---
+++ test/Herbert.Data.Tests/Repositories/ApplicationUserRepositoryTests.cs
@@ -0,0 +1,62 @@
+﻿namespace Herbert.Data.Tests.Repositories
+{
+    using System;
+    using Xunit;
+
+    using Herbert.Data.Repositories;
+    using Herbert.Model.UserInfo;
+
+    public class ApplicationUserRepositoryTests : RepositoryTestBase
+    {
+        [Fact(DisplayName = "Can add multiple user")]
+        public void TestAddNewUser()
+        {
+            using (var context = new HerbertContext(CreateNewContextOptions()))
+            {
+                var repository = new ApplicationUserRepository(context);
+
+                repository.AddNewUser("email1@email.com", "encryptPassword", "BigEgg", RegisterSourceType.Website);
+
+                repository.AddNewUser("email2@email.com", "encryptPassword", "BigEgg", RegisterSourceType.Website);
+            }
+        }
+
+        [Fact(DisplayName = "Should not add a user when Email already exist")]
+        public void TestAddNewUser_DuplicateEmail()
+        {
+            using (var context = new HerbertContext(CreateNewContextOptions()))
+            {
+                var repository = new ApplicationUserRepository(context);
+
+                repository.AddNewUser("email@email.com", "encryptPassword", "BigEgg", RegisterSourceType.Website);
+
+                Assert.Throws<InvalidOperationException>(() => 
+                    repository.AddNewUser("email@email.com", "encryptPassword", "BigEgg", RegisterSourceType.Website)
+                );
+            }
+        }
+
+        [Fact(DisplayName = "Can store the user info into DB correctly")]
+        public void TestGetUser()
+        {
+            using (var context = new HerbertContext(CreateNewContextOptions()))
+            {
+                var repository = new ApplicationUserRepository(context);
+
+                repository.AddNewUser("email@email.com", "encryptPassword", "BigEgg", RegisterSourceType.Website);
+
+                var user = repository.GetUser("email@email.com");
+
+                Assert.NotNull(user.Id);
+                Assert.Equal("email@email.com", user.Email, false);
+                Assert.Equal("encryptPassword", user.Password, false);
+                Assert.Equal("BigEgg", user.NickName, false);
+                Assert.Equal(RegisterSourceType.Website, user.RegisterSource);
+                Assert.Equal(UserRole.User, user.Role);
+
+                Assert.True(user.CreatedTime > DateTime.UtcNow.AddMinutes(-10), "Created Time should be set.");
+                Assert.True(user.LastUpdated > DateTime.UtcNow.AddMinutes(-10), "Last Updated Time should be set.");
+            }
+        }
+    }
+}
---
+++ test/Herbert.Data.Tests/Repositories/RepositoryTestBase.cs
@@ -0,0 +1,25 @@
+﻿namespace Herbert.Data.Tests.Repositories
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public abstract class RepositoryTestBase
+    {
+        protected static DbContextOptions<HerbertContext> CreateNewContextOptions()
+        {
+            // Create a fresh service provider, and therefore a fresh 
+            // InMemory database instance.
+            var serviceProvider = new ServiceCollection()
+                .AddEntityFrameworkInMemoryDatabase()
+                .BuildServiceProvider();
+
+            // Create a new options instance telling the context to use an
+            // InMemory database and the new service provider.
+            var builder = new DbContextOptionsBuilder<HerbertContext>();
+            builder.UseInMemoryDatabase()
+                   .UseInternalServiceProvider(serviceProvider);
+
+            return builder.Options;
+        }
+    }
+}
