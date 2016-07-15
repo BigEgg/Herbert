d89ab9916610b564785a2386c3ff3c743f759f9b
BigEgg <cd_bigegg@hotmail.com>
2016-07-15 20:29:53 +0800

    Add DB Context and Application User Entity's contracts

---
+++ src/Herbert.Data/EntityConfigurations/ApplicationUserConfiguation.cs
@@ -0,0 +1,39 @@
+﻿namespace Herbert.Data.EntityConfigurations
+{
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using Herbert.Model.UserInfo;
+
+    public static class ApplicationUserConfiguation
+    {
+        /// <summary>
+        /// Setups the contracts for <see cref="ApplicationUser"/> entity.
+        /// </summary>
+        /// <param name="entityBuilder">The entity builder.</param>
+        public static void SetupContracts(this EntityTypeBuilder<ApplicationUser> entityBuilder)
+        {
+            entityBuilder.HasKey(u => u.Id);
+
+            entityBuilder.HasAlternateKey(u => u.Email);
+
+            entityBuilder.HasIndex(u => u.Email);
+
+            entityBuilder.Property(u => u.Email)
+                .IsRequired()
+                .HasMaxLength(255)
+                .IsConcurrencyToken();
+
+            entityBuilder.Property(u => u.Password)
+                .IsRequired();
+
+            entityBuilder.Property(u => u.NickName)
+                .IsRequired();
+
+            entityBuilder.Property(u => u.CreatedTime)
+                .ValueGeneratedOnAdd();
+
+            entityBuilder.Property(u => u.LastUpdated)
+                .ValueGeneratedOnAddOrUpdate();
+        }
+    }
+}
