b0ab96284d00b92f1c66397f8e96f155f1f7670d
BigEgg <cd_bigegg@hotmail.com>
2016-07-17 17:42:03 +0800

    Add support application model

--- src/Herbert.Models/UserInfo/ApplicationUser.cs
+++ src/Herbert.Models/UserInfo/ApplicationUser.cs
@@ -8,42 +8,42 @@
     public class ApplicationUser
     {
         /// <summary>
-        /// Get and set the GUID of this user
+        /// Get or set the GUID of this user
         /// </summary>
         public Guid Id { get; set; }
 
         /// <summary>
-        /// Get and set the email address of this user, this will need when sign in
+        /// Get or set the email address of this user, this will need when sign in
         /// </summary>
         public string Email { get; set; }
 
         /// <summary>
-        /// Get and set the password
+        /// Get or set the password
         /// </summary>
         public string Password { get; set; }
 
         /// <summary>
-        /// Get and set the nick name of user
+        /// Get or set the nick name of user
         /// </summary>
         public string NickName { get; set; }
 
         /// <summary>
-        /// Get and set the nick name of user
+        /// Get or set the nick name of user
         /// </summary>
         public UserRole Role { get; set; }
         
         /// <summary>
-        /// Get and set the register source type of user
+        /// Get or set the register source type of user
         /// </summary>
         public RegisterSourceType RegisterSource  { get; set; }
 
         /// <summary>
-        /// Get and set the <see cref="DateTime"/> when this user created.
+        /// Get or set the <see cref="DateTime"/> when this user created.
         /// </summary>
         public DateTime CreatedTime { get; set; }
 
         /// <summary>
-        /// Get and set the latest <see cref="DateTime"/> when this user update information.
+        /// Get or set the latest <see cref="DateTime"/> when this user update information.
         /// </summary>
         public DateTime LastUpdated { get; set; }
     }
