18a83d62c8b7d560e2db451401ae8cee08cdbde2
BigEgg <cd_bigegg@hotmail.com>
2016-07-17 18:12:08 +0800

    Add tests for Support Application repository

---
+++ test/Herbert.DAL.Tests/Repositories/SupportApplicationRepositoryTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Herbert.DAL.Tests.Repositories
+{
+    using Xunit;
+
+    using Herbert.DAL.Repositories;
+    using Herbert.Models.Access;
+    using System.Linq;
+
+    public class SupportApplicationRepositoryTests
+    {
+        [Fact(DisplayName = "Can get all support application as a list")]
+        public void TestGetAll()
+        {
+            using (var context = new TestHerbertContext())
+            {
+                var respository = new SupportApplicationRepository(context);
+
+                var supportApplications = respository.GetAll();
+                Assert.True(supportApplications.Any(a => a.ApplicationType == SupportApplicationType.Website));
+            }
+        }
+    }
+}
