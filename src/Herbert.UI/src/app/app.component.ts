3798373892bb323d462af9e2d88073fbeb421b9c
BigEgg <cd_bigegg@hotmail.com>
2016-07-20 20:29:25 +0800

    Setup the project with Angular & Angular Material

--- src/Herbert.UI/src/app/app.component.ts
+++ src/Herbert.UI/src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
+import { MD_BUTTON_DIRECTIVES } from '@angular2-material/button';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [MD_CARD_DIRECTIVES, MD_BUTTON_DIRECTIVES]
 })
+
 export class AppComponent {
   title = 'app works!';
 }
