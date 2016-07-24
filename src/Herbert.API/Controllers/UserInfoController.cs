e5d8b7ff575100b96321fc985765c0d7685351ec
BigEgg <cd_bigegg@hotmail.com>
2016-07-24 22:24:04 +0800

    Add API for checking is email already used

---
+++ src/Herbert.API/Controllers/UserInfoController.cs
@@ -0,0 +1,27 @@
+﻿namespace Herbert.API.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using ViewModels.UserInfo;
+    using Services.UserInfo;
+    using System.Net;
+
+    [Route("api/user-info")]
+    public class UserInfoController : Controller
+    {
+        private readonly IApplicationUserService applicationUserService;
+
+        public UserInfoController(IApplicationUserService applicationUserService)
+        {
+            this.applicationUserService = applicationUserService;
+        }
+
+        // GET api/user-info/check-email
+        [HttpGet("check-email", Name = "CheckEmail")]
+        public IActionResult CheckEmail([FromBody] CheckEmailRequest request)
+        {
+            if (ModelState.IsValid) { return BadRequest(ModelState); }
+
+            return Ok(applicationUserService.IsEmailAlreadyUsed(request.Email));
+        }
+    }
+}
---
+++ src/Herbert.API/ViewModels/UserInfo/CheckEmailRequestValidator.cs
@@ -0,0 +1,24 @@
+﻿namespace Herbert.API.ViewModels.UserInfo
+{
+    using FluentValidation;
+
+    /// <summary>
+    /// The validator class for <see cref="CheckEmailRequest"/> class.
+    /// </summary>
+    /// <seealso cref="FluentValidation.AbstractValidator{EmailCheckRequest}" />
+    internal class CheckEmailRequestValidator : AbstractValidator<CheckEmailRequest>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CheckEmailRequestValidator"/> class.
+        /// </summary>
+        public CheckEmailRequestValidator()
+        {
+            CascadeMode = CascadeMode.StopOnFirstFailure;
+
+            RuleFor(viewModel => viewModel.Email)
+                .NotEmpty()
+                .EmailAddress()
+                .Length(256);
+        }
+    }
+}
