3edaf0c1ac7495588eb3420a378524694d366f5e
BigEgg <cd_bigegg@hotmail.com>
2016-08-01 17:29:32 +0800

    Fix TypeScript Lint Error

--- src/Herbert.UI/src/main.ts
+++ src/Herbert.UI/src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
-import { disableDeprecatedForms, provideForms } from '@angular/forms'; 
+import { disableDeprecatedForms, provideForms } from '@angular/forms';
 
 if (environment.production) {
   enableProdMode();
