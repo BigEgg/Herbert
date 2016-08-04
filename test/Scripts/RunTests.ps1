c1ec16b769b529a5a24ee76ca7ac497699a9702b
BigEgg <cd_bigegg@hotmail.com>
2016-08-04 22:12:17 +0800

    Update PowerShell scripts for running tests for API layer

--- test/Scripts/RunTests.ps1
+++ test/Scripts/RunTests.ps1
@@ -3,20 +3,25 @@ param (
 )
 
 IF ($environment) {
-    WRITE-OUTPUT "Run Test with $environment Enviroment"
+    WRITE-HOST "Run Test with $environment Enviroment"
     $env:TEST_ENVIRONMENT = "$environment"
 } ELSE {
-    WRITE-OUTPUT "Run Test in local with Development Enviroment"
+    WRITE-HOST "Run Test in local with Development Enviroment"
 }
 
-WRITE-OUTPUT "Run Tests for DAL"
+WRITE-HOST "Run Tests for DAL" -ForegroundColor "Yellow"
 CD ..\Herbert.DAL.Tests
 dotnet test
-WRITE-OUTPUT "Run Tests for DAL Done"
+WRITE-HOST "Run Tests for DAL Done" -ForegroundColor "Cyan"
 
-WRITE-OUTPUT "Run Tests for Business Logic"
+WRITE-HOST "Run Tests for Business Logic" -ForegroundColor "Yellow"
 CD ..\Herbert.Services.Tests
 dotnet test
-WRITE-OUTPUT "Run Tests for Business Logic Done"
+WRITE-HOST "Run Tests for Business Logic Done" -ForegroundColor "Cyan"
+
+WRITE-HOST "Run Tests for API Layer" -ForegroundColor "Yellow"
+CD ..\Herbert.API.Tests
+dotnet test
+WRITE-HOST "Run Tests for API Layer Done" -ForegroundColor "Cyan"
 
 CD ..\Scripts
