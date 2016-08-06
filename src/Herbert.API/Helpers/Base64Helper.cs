87249e65e964e08a1306453cc2511fd5ff15537c
BigEgg <cd_bigegg@hotmail.com>
2016-08-07 00:05:33 +0800

    Add extension methods for Base64 Encode/Decode

---
+++ src/Herbert.API/Helpers/Base64Helper.cs
@@ -0,0 +1,37 @@
+﻿namespace Herbert.API.Helpers
+{
+    using System;
+    using System.Text;
+
+    /// <summary>
+    /// The string Base64 Encode/Decode extensions
+    /// </summary>
+    public static class Base64Helper
+    {
+        /// <summary>
+        /// Decode Base64 format string to raw message.
+        /// </summary>
+        /// <param name="base64String">The base64 format string.</param>
+        /// <returns>The raw message.</returns>
+        public static string Base64Decode(this string base64String)
+        {
+            if (string.IsNullOrWhiteSpace(base64String)) { return string.Empty; }
+
+            byte[] bytes = Convert.FromBase64String(base64String);
+            return Encoding.ASCII.GetString(bytes);
+        }
+
+        /// <summary>
+        /// Encode message to Base64 format string.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <returns>The Base 64 format string.</returns>
+        public static string Base64Encode(this string message)
+        {
+            if (string.IsNullOrWhiteSpace(message)) { return string.Empty; }
+
+            byte[] bytes = Encoding.ASCII.GetBytes(message);
+            return Convert.ToBase64String(bytes);
+        }
+    }
+}
