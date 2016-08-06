f5b738d60176999b4131efe7d0a18b7589d7c476
BigEgg <cd_bigegg@hotmail.com>
2016-08-07 00:06:54 +0800

    Add action filter for check is request from authorized application.

---
+++ src/Herbert.API/Controllers/Filters/ApplicationAuthorizeAttribute.cs
@@ -0,0 +1,85 @@
+﻿namespace Herbert.API.Controllers.Filters
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using Microsoft.Extensions.Primitives;
+    using System;
+    using System.Linq;
+
+    using Herbert.API.Helpers;
+    using Herbert.API.ViewModels.Access;
+    using Herbert.Models.Access;
+    using Herbert.Services.Access;
+
+    /// <summary>
+    /// The Attribute class for check is valid Application to call this API
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.Filters.ActionFilterAttribute" />
+    public class ApplicationAuthorizeAttribute : ActionFilterAttribute
+    {
+        private const string APP_AUTHORIZE_SOURCE_TYPE_KEY = "X-Herbert-ClientSource";
+        private const string APP_AUTHORIZE_TOKEN_KEY = "X-Herbert-Authenticate";
+        private readonly SupportApplicationService supportApplicationService;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ApplicationAuthorize"/> class.
+        /// </summary>
+        /// <param name="supportApplicationService">The support application service.</param>
+        public ApplicationAuthorizeAttribute(SupportApplicationService supportApplicationService)
+        {
+            this.supportApplicationService = supportApplicationService;
+        }
+
+
+        /// <summary>
+        /// Check request is from authorized application or not.
+        /// </summary>
+        /// <param name="actionContext">The action context.</param>
+        public override void OnActionExecuting(ActionExecutingContext actionContext)
+        {
+            var typeRaw = GetHeaderValue(actionContext.HttpContext.Request, APP_AUTHORIZE_SOURCE_TYPE_KEY);
+            var tokenRaw = GetHeaderValue(actionContext.HttpContext.Request, APP_AUTHORIZE_TOKEN_KEY);
+            if (string.IsNullOrEmpty(typeRaw) || string.IsNullOrWhiteSpace(tokenRaw))
+            {
+                actionContext.Result = new UnauthorizedResult();
+                return;
+            }
+
+            SupportApplicationType type;
+            if (!Enum.TryParse(typeRaw, out type))
+            {
+                actionContext.Result = new UnauthorizedResult();
+                return;
+            }
+
+            var token = tokenRaw.Base64Decode().Deserialize<ApplicationTokenVM>();
+            if (token == null)
+            {
+                actionContext.Result = new UnauthorizedResult();
+                return;
+            }
+
+            var applicationType = supportApplicationService.GetApplicationType(token.AppId, token.AppSecret);
+            if (!applicationType.HasValue || applicationType.Value != type)
+            {
+                actionContext.Result = new UnauthorizedResult();
+                return;
+            }
+        }
+
+        private string GetHeaderValue(HttpRequest request, string key)
+        {
+            var values = request.Headers.GetCommaSeparatedValues(key);
+
+            if (StringValues.IsNullOrEmpty(values))
+            {
+                return string.Empty;
+            }
+            else
+            {
+                return values.First();
+            }
+        }
+    }
+}
---
+++ src/Herbert.API/ViewModels/Access/ApplicationTokenVM.cs
@@ -0,0 +1,27 @@
+﻿namespace Herbert.API.ViewModels.Access
+{
+    using System;
+
+    /// <summary>
+    /// The Application Token View Model
+    /// </summary>
+    public class ApplicationTokenVM
+    {
+        /// <summary>
+        /// Gets or sets the support application identifier.
+        /// </summary>
+        /// <value>
+        /// The support application identifier.
+        /// </value>
+        public Guid AppId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the support application secret.
+        /// </summary>
+        /// <value>
+        /// The support application secret.
+        /// </value>
+        public string AppSecret { get; set; }
+
+    }
+}
