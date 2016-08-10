4952a07d704c80bc082d0e9f6ec69848548d220c
BigEgg <cd_bigegg@hotmail.com>
2016-08-10 23:05:34 +0800

    Add model class for User Access Token

---
+++ src/Herbert.Models/Access/UserAccessToken.cs
@@ -0,0 +1,58 @@
+﻿namespace Herbert.Models.Access
+{
+    using System;
+
+    /// <summary>
+    /// The access token for user log in
+    /// </summary>
+    public class UserAccessToken
+    {
+        /// <summary>
+        /// Gets or sets the access token.
+        /// </summary>
+        /// <value>
+        /// The access token.
+        /// </value>
+        public string AccessToken { get; set; }
+
+        /// <summary>
+        /// Gets or sets the access token expired time.
+        /// </summary>
+        /// <value>
+        /// The access token expired time.
+        /// </value>
+        public DateTime AccessTokenExpiredTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the access token for application type.
+        /// </summary>
+        /// <value>
+        /// The application type.
+        /// </value>
+        public SupportApplicationType ApplicationType { get; set; }
+
+        /// <summary>
+        /// Gets or sets the refresh token.
+        /// </summary>
+        /// <value>
+        /// The refresh token.
+        /// </value>
+        public string RefreshToken { get; set; }
+
+        /// <summary>
+        /// Gets or sets the refresh token expired time.
+        /// </summary>
+        /// <value>
+        /// The refresh token expired time.
+        /// </value>
+        public DateTime RefreshTokenExpiredTime { get; set; }
+
+        /// <summary>
+        /// Gets or set the GUID of this user
+        /// </summary>
+        /// <value>
+        /// The GUID of this user.
+        /// </value>
+        public Guid UserId { get; set; }
+    }
+}
--- src/Herbert.Models/UserInfo/ApplicationUser.cs
+++ src/Herbert.Models/UserInfo/ApplicationUser.cs
@@ -8,42 +8,42 @@
     public class ApplicationUser
     {
         /// <summary>
-        /// Get or set the GUID of this user
+        /// Gets or sets the GUID of this user
         /// </summary>
         public Guid Id { get; set; }
 
         /// <summary>
-        /// Get or set the email address of this user, this will need when sign in
+        /// Gets or sets the email address of this user, this will need when sign in
         /// </summary>
         public string Email { get; set; }
 
         /// <summary>
-        /// Get or set the password
+        /// Gets or sets the password
         /// </summary>
         public string Password { get; set; }
 
         /// <summary>
-        /// Get or set the nick name of user
+        /// Gets or sets the nick name of user
         /// </summary>
         public string NickName { get; set; }
 
         /// <summary>
-        /// Get or set the nick name of user
+        /// Gets or sets the nick name of user
         /// </summary>
         public UserRole Role { get; set; }
         
         /// <summary>
-        /// Get or set the register source type of user
+        /// Gets or sets the register source type of user
         /// </summary>
         public RegisterSourceType RegisterSource  { get; set; }
 
         /// <summary>
-        /// Get or set the <see cref="DateTime"/> when this user created.
+        /// Gets or sets the <see cref="DateTime"/> when this user created.
         /// </summary>
         public DateTime CreatedTime { get; set; }
 
         /// <summary>
-        /// Get or set the latest <see cref="DateTime"/> when this user update information.
+        /// Gets or sets the latest <see cref="DateTime"/> when this user update information.
         /// </summary>
         public DateTime LastUpdated { get; set; }
     }
