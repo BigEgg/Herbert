89fe5684ba86426bb4ba93f662453938af1b8e2e
BigEgg <cd_bigegg@hotmail.com>
2016-08-10 23:07:20 +0800

    Add the fake class for Access Token cache by Redis

---
+++ src/Herbert.Services/Access/AccessTokenRedisCache.cs
@@ -0,0 +1,44 @@
+﻿namespace Herbert.Services.Access
+{
+    using System;
+
+    using Herbert.Models.Access;
+
+    /// <summary>
+    /// The Redis Cache repository for <see cref="UserAccessToken"/> model
+    /// </summary>
+    /// <seealso cref="IAccessTokenCache" />
+    public class AccessTokenRedisCache : IAccessTokenCache
+    {
+        /// <summary>
+        /// Create a new access token.
+        /// </summary>
+        /// <param name="userAccessToken">The user access token.</param>
+        public void AddAccessToken(UserAccessToken userAccessToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Gets the user access token by specific access token.
+        /// </summary>
+        /// <param name="accessToken">The access token.</param>
+        /// <returns>
+        /// The user access token
+        /// </returns>
+        public UserAccessToken GetAccessToken(string accessToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Updates the access token.
+        /// </summary>
+        /// <param name="oldAccessToken">The old access token.</param>
+        /// <param name="newAccessToken">The new access token.</param>
+        public void UpdateAccessToken(string oldAccessToken, string newAccessToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
